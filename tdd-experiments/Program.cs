b8d0d93ccf4cf9e575a80eddf317f407c36179ab
catiaraminhos <catia.s.raminhos@gmail.com>
2021-02-11 16:21:50 +0000

    Init Visual Studio project

---
+++ tdd-experiments/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tdd_experiments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
