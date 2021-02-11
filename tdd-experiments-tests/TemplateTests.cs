3be6199bf91180e0024bfb5c5ef6828a7881ee85
catiaraminhos <catia.s.raminhos@gmail.com>
2021-02-11 16:28:48 +0000

    Implement basic test cases and functionality of Template
    Test cases:
    - Evaluating template “Hello, ${name}” with the value “Reader” for variable
    “name” results in the string “Hello, Reader”.
    - Evaluate template “${one}, ${two}, ${three}” with values “1”, “2”,
    and “3”, respectively, and verify that the template engine renders the result as
    “1, 2, 3”.
    - Evaluating template “Hello, ${name}” with values “Hi” and “Reader” for variables “doesnotexist” and “name”, respectively, results in the string “Hello,
    Reader”.

---
+++ tdd-experiments-tests/TemplateTests.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using TemplateEngine;
+
+namespace TemplateTests
+{
+    [TestFixture]
+    public class TemplateTests
+    {
+        [Test]
+        public void OneVariable()
+        {
+            Template template = new Template("Hello, ${name}");
+            template.Set("name", "Reader");
+            Assert.That(template.Evaluate(), Is.EqualTo("Hello, Reader"));
+        }
+
+
+        [Test]
+        public void DifferentTemplate()
+        {
+            Template template = new Template("Hi, ${name}");
+            template.Set("name", "John");
+            Assert.That(template.Evaluate(), Is.EqualTo("Hi, John"));
+        }
+
+
+        [Test]
+        public void MultipleVariables()
+        {
+            Template template = new Template("${one}, ${two}, ${three}");
+            template.Set("one", "1");
+            template.Set("two", "2");
+            template.Set("three", "3");
+            Assert.That(template.Evaluate(), Is.EqualTo("1, 2, 3"));
+        }
+
+        [Test]
+        public void UnknownVariablesAreIgnored()
+        {
+            Template template = new Template("Hello, ${name}");
+            template.Set("name", "Reader");
+            template.Set("doesnotexist", "Hi");
+            Assert.That(template.Evaluate(), Is.EqualTo("Hello, Reader"));
+        }
+    }
+}
---
+++ tdd-experiments/Template.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace TemplateEngine
+{
+    public class Template
+    {
+        private string templateText;
+
+        private Dictionary<string, string> variables;
+
+
+        public Template(string templateText)
+        {
+            this.templateText = templateText;
+            this.variables = new Dictionary<string, string>();
+        }
+
+        public void Set(string variable, string value)
+        {
+            this.variables.Add(variable, value);
+        }
+
+        public string Evaluate()
+        {
+            string templateResult = this.templateText;
+
+            foreach(KeyValuePair<string, string> variable in this.variables)
+            {
+                string variableNameRegex = "\\$\\{" + variable.Key + "\\}";
+                templateResult = Regex.Replace(templateResult, variableNameRegex , variable.Value);
+            }
+
+            return templateResult;
+        }
+    }
+}
