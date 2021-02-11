ece650e2c13716688aa741058b26c535f68cbd8d
catiaraminhos <catia.s.raminhos@gmail.com>
2021-02-11 16:41:36 +0000

    Refactor Template tests

--- tdd-experiments-tests/TemplateTests.cs
+++ tdd-experiments-tests/TemplateTests.cs
@@ -6,41 +6,36 @@ namespace TemplateTests
     [TestFixture]
     public class TemplateTests
     {
-        [Test]
-        public void OneVariable()
-        {
-            Template template = new Template("Hello, ${name}");
-            template.Set("name", "Reader");
-            Assert.That(template.Evaluate(), Is.EqualTo("Hello, Reader"));
-        }
+        private Template template;
 
 
-        [Test]
-        public void DifferentTemplate()
+        [SetUp]
+        public void SetUp()
         {
-            Template template = new Template("Hi, ${name}");
-            template.Set("name", "John");
-            Assert.That(template.Evaluate(), Is.EqualTo("Hi, John"));
+            template = new Template("${one}, ${two}, ${three}");
+            template.Set("one", "1");
+            template.Set("two", "2");
+            template.Set("three", "3");
         }
 
 
         [Test]
         public void MultipleVariables()
         {
-            Template template = new Template("${one}, ${two}, ${three}");
-            template.Set("one", "1");
-            template.Set("two", "2");
-            template.Set("three", "3");
-            Assert.That(template.Evaluate(), Is.EqualTo("1, 2, 3"));
+            AssertTemplateEvaluatesTo("1, 2, 3");
         }
 
         [Test]
         public void UnknownVariablesAreIgnored()
         {
-            Template template = new Template("Hello, ${name}");
-            template.Set("name", "Reader");
-            template.Set("doesnotexist", "Hi");
-            Assert.That(template.Evaluate(), Is.EqualTo("Hello, Reader"));
+            template.Set("doesnotexist", "whatever");
+            AssertTemplateEvaluatesTo("1, 2, 3");
+        }
+
+
+        private void AssertTemplateEvaluatesTo(string expected)
+        {
+            Assert.That(template.Evaluate(), Is.EqualTo(expected));
         }
     }
 }
