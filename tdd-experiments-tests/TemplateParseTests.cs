b94868ad046ea55e8eecd5f19c310ac2444949f4
catiaraminhos <catia.s.raminhos@gmail.com>
2021-02-12 18:40:58 +0000

    Set up class to parse a template, add use case for only plain text and comment failing test

---
+++ tdd-experiments-tests/TemplateParseTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using TemplateEngine;
+
+namespace TemplateTests
+{
+    [TestFixture]
+    class TemplateParseTests
+    {
+        [Test]
+        public void EmptyTemplateParsesAsEmptyString()
+        {
+            var parse = new TemplateParse();
+            List<string> segments = parse.Parse("");
+            Assert.That(segments.Count, Is.EqualTo(1));
+            Assert.That(segments[0], Is.EqualTo(""));
+        }
+
+        [Test]
+        public void TemplateWithOnlyPlainText()
+        {
+            var parse = new TemplateParse();
+            List<string> segments = parse.Parse("plain text only");
+            Assert.That(segments.Count, Is.EqualTo(1));
+            Assert.That(segments[0], Is.EqualTo("plain text only"));
+        }
+    }
+}
---
+++ tdd-experiments/TemplateParse.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace TemplateEngine
+{
+    public class TemplateParse
+    {
+        public List<string> Parse(string templateText)
+        {
+            var parsedTemplate = new List<string>();
+            parsedTemplate.Add(templateText);
+            return parsedTemplate;
+        }
+    }
+}
