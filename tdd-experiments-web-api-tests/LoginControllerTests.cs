1ec27ff0aba7a205eb97e4c059b73620dc048a94
catiaraminhos <catia.s.raminhos@gmail.com>
2021-02-22 18:49:57 +0000

    Handle wrong login

---
+++ tdd-experiments-web-api-tests/LoginControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using Microsoft.AspNetCore.Mvc;
+using TDDWebAPI.Controllers;
+
+namespace TDDWebAPITests
+{
+    [TestFixture]
+    public class LoginControllerTests
+    {
+        [Test]
+        public void WrongPasswordShouldRedirectToErrorPage()
+        {
+            LoginController loginController = new LoginController();
+            var redirectResult = loginController.Login("nonsuchuser", "wrongpassword") as RedirectResult;
+            Assert.That(redirectResult.Url, Is.EqualTo("/invalidlogin"));
+        }
+    }
+}
---
+++ tdd-experiments-web-api/Controllers/LoginController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TDDWebAPI.Controllers
+{
+    [Route("api/login")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Login(string username, string password)
+        {
+            var redirectResult = Redirect("/invalidlogin");
+            return redirectResult;
+        }
+    }
+}
